cd6ce314091781ee8aca13fb1de3156f871ac4a9
pid011 <pid011@kakao.com>
2021-05-09 02:26:11 +0900

    Add Lecture09 projects

---
+++ Lecture09/InventoryCopy/InventoryCopy.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+
+class Item
+{
+public:
+    int number;
+};
+
+class Inventory
+{
+public:
+    explicit Inventory(int itemNum)
+    {
+        _items = new Item[itemNum];
+    }
+
+    Inventory(const Inventory& rhs)
+    {
+        size_t size = _msize(rhs._items) / sizeof(Item);
+        _items = new Item[size];
+        for (size_t i = 0; i < size; i++)
+        {
+            _items[i] = rhs._items[i];
+        }
+    }
+
+    Inventory(Inventory&& rhs) noexcept
+    {
+        size_t size = _msize(rhs._items) / sizeof(Item);
+        _items = new Item[size];
+        for (size_t i = 0; i < size; i++)
+        {
+            _items[i] = rhs._items[i];
+        }
+
+        delete[] rhs._items;
+        rhs._items = nullptr;
+    }
+
+    Inventory& operator=(const Inventory& rhs)
+    {
+        if (&rhs == this) return *this;
+
+        Item* originItems = _items;
+        size_t size = _msize(rhs._items) / sizeof(Item);
+
+        if (size > 0)
+        {
+            _items = new Item[size];
+            for (size_t i = 0; i < size; i++)
+            {
+                _items[i] = rhs._items[i];
+            }
+        }
+
+        delete[] originItems;
+        return *this;
+    }
+
+    Inventory& operator=(Inventory&& rhs) noexcept
+    {
+        if (&rhs == this) return *this;
+
+        Item* originItems = _items;
+        size_t size = _msize(rhs._items) / sizeof(Item);
+
+        if (size > 0)
+        {
+            _items = new Item[size];
+            for (size_t i = 0; i < size; i++)
+            {
+                _items[i] = rhs._items[i];
+            }
+        }
+
+        delete[] originItems;
+
+        delete[] rhs._items;
+        rhs._items = nullptr;
+
+        return *this;
+    }
+
+    size_t Size() const
+    {
+        if (_items == nullptr) return 0;
+
+        return _msize(_items) / sizeof(Item);
+    }
+
+    ~Inventory()
+    {
+        delete[] _items;
+        _items = nullptr;
+    }
+
+private:
+    Item* _items = nullptr;
+};
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+
+    Inventory playerInven(10);
+    cout << "player inven size: " << playerInven.Size() << endl;
+
+    Inventory npcInven(5);
+    cout << "npc inven size: " << npcInven.Size() << endl;
+
+    cout << endl;
+
+    playerInven = npcInven;
+    cout << "player inven size: " << playerInven.Size() << endl;
+    cout << "npc inven size: " << npcInven.Size() << endl;
+
+    cout << endl;
+
+    npcInven = Inventory(20);
+    cout << "npc inven size: " << npcInven.Size() << endl;
+
+    cout << endl;
+
+    playerInven = std::move(npcInven);
+    cout << "player inven size: " << playerInven.Size() << endl;
+    cout << "npc inven size: " << npcInven.Size() << endl;
+}
+
+/*
+player inven size: 10
+npc inven size: 5
+
+player inven size: 5
+npc inven size: 5
+
+npc inven size: 20
+
+player inven size: 20
+npc inven size: 0
+*/
---
+++ Lecture09/MemoryBlock/MemoryBlock.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class MemoryBlock
+{
+public:
+    explicit MemoryBlock(size_t length) : _length(length), _data(new int[length])
+    {
+        std::cout << "Constructor Length: " << _length << std::endl;
+    }
+
+    ~MemoryBlock()
+    {
+        std::cout << "Destructor Length: " << _length << std::endl;
+        if (_data != nullptr)
+        {
+            std::cout << "Deleting resource." << std::endl;
+            delete[] _data;
+        }
+    }
+
+    MemoryBlock(const MemoryBlock& other) : _length(other._length), _data(new int[other._length])
+    {
+        std::cout << "Copy Constructor Length: " << _length << std::endl;
+        memcpy_s(_data, _length, other._data, _length);
+    }
+
+    MemoryBlock& operator=(const MemoryBlock& other)
+    {
+        std::cout << "Copy assignment operator Length: " << other._length << std::endl;
+        if (this != &other)
+        {
+            delete[] _data;
+            _length = other._length;
+            _data = new int[_length];
+            memcpy_s(_data, _length, other._data, _length);
+        }
+        return *this;
+    }
+
+    MemoryBlock(MemoryBlock&& other) noexcept : _data(nullptr), _length(0)
+    {
+        std::cout << "In MemoryBlock(MemoryBlock&&).length = " << other._length << ". Moving resource." << std::endl;
+
+        _data = other._data;
+        _length = other._length;
+
+        other._data = nullptr;
+        other._length = 0;
+    }
+
+    MemoryBlock& operator=(MemoryBlock&& other) noexcept
+    {
+        std::cout << "In operator=(MemoryBlock&&).length = " << other._length << "." << std::endl;
+
+        if (this != &other)
+        {
+            delete[] _data;
+            _data = other._data;
+            _length = other._length;
+
+            other._data = nullptr;
+            other._length = 0;
+        }
+        return *this;
+    }
+
+    size_t Length() const
+    {
+        return _length;
+    }
+
+private:
+    size_t _length;
+    int* _data;
+};
+
+int main()
+{
+    std::vector<MemoryBlock> vTemp;
+    vTemp.push_back(MemoryBlock(50));
+    std::cout << "====================" << std::endl;
+    vTemp.push_back(MemoryBlock(100));
+    std::cout << "====================" << std::endl;
+    vTemp[0] = MemoryBlock(75);
+    std::cout << "====================" << std::endl;
+    return 0;
+}
